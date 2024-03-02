4b3e1a1afe76c2e7f5a04d49ac37d8c2213ed6c8
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-03-01 19:35:53 -0600

    Initial Commit

---
+++ tableViewFlashCardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewFlashCardApp
+//
+//  Created by Tabita Sadiq on 3/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
